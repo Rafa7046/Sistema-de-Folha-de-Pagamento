3bf4d25b58049fac6d8323a7798c735629f6bf2e
rafa7046 <rafael-vilella@hotmail.com>
2021-07-21 15:00:54 -0300

    pickle

---
+++ format.py
@@ -0,0 +1,21 @@
+import dill
+import pickle
+from employee import employees
+
+employee_json = {}
+
+# def format_employee():
+#     for x in employees:
+#         employee_json['data'] = []
+#         employee_json['data'].append({
+#             'name': x.name ,
+#             'address': x.address ,
+#             'type_of_worker': x.type_of_worker ,
+#             'type_of_payment': x.type_of_payment ,
+#             'syndicate': x.syndicate ,
+#             'Id': x.Id ,
+#         })
+
+def save_object():
+    with open('data.pkl', 'wb') as output:
+        dill.dump(employees, output, pickle.HIGHEST_PROTOCOL)
